1d7a0271b721d6e7a3c90c6a918232bf298bcf22
kyrylo <kyrylo.onufriiev@applitools.com>
2023-05-22 15:20:36 +0300

    feat: new feature

--- js/packages/driver/src/driver.ts
+++ js/packages/driver/src/driver.ts
@@ -23,6 +23,8 @@ import * as utils from '@applitools/utils'
 
 const snippets = require('@applitools/snippets')
 
+// THIS IS NEW FEATURE
+
 type DriverState<T extends SpecType> = {
   nmlElement?: Element<T> | null
 }
