3da7e1ad24f635ec15f13a14aef0df6eb4b9c5e7
kyrylo <kyrylo.onufriiev@applitools.com>
2023-05-23 10:42:44 +0300

    chore: add logs

--- .github/actions/release-please/plugins/node-improved-plugin.ts
+++ .github/actions/release-please/plugins/node-improved-plugin.ts
@@ -1,5 +1,10 @@
 import {NodeWorkspace} from 'release-please/build/src/plugins/node-workspace'
+import {CandidateReleasePullRequest, RepositoryConfig} from 'release-please/build/src/manifest';
 
 export class NodeImprovedWorkspace extends NodeWorkspace {
-  
+  protected async buildAllPackages(candidates: CandidateReleasePullRequest[]) {
+    const res = await super.buildAllPackages(candidates)
+    console.log(res)
+    return res
+  }
 }
