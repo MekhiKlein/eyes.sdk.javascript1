038d6082527b5edd85efee19ad1aa4b968f52a37
kyrylo <kyrylo.onufriiev@applitools.com>
2023-05-23 12:17:11 +0300

    chore: fix plugin

--- .github/actions/release-please/plugins/node-improved-plugin.ts
+++ .github/actions/release-please/plugins/node-improved-plugin.ts
@@ -2,15 +2,9 @@ import {NodeWorkspace} from 'release-please/build/src/plugins/node-workspace'
 import {CandidateReleasePullRequest, RepositoryConfig} from 'release-please/build/src/manifest';
 
 export class NodeImprovedWorkspace extends NodeWorkspace {
-  // protected async buildAllPackages(candidates: CandidateReleasePullRequest[]) {
-  //   const res = await super.buildAllPackages(candidates)
-  //   console.log(res)
-  //   return res
-  // }
   protected newCandidate(a: any, b: any) {
     const c = super.newCandidate(a, b)
-    console.log(c, this.repositoryConfig)
-    c.config = {...c.config}
+    c.config = {...c.config, ...this.repositoryConfig[c.path]}
     return c
   }
 }
