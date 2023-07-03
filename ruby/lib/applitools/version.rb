2ca809ee2960473eeeb74029b9b772f52a8f80d0
Serhii Ovchynnyk <sergii.ovchynnyk@applitools.com>
2023-07-03 23:32:39 +0300

    ruby : update CHANGELOG and Bump version

--- ruby/lib/applitools/version.rb
+++ ruby/lib/applitools/version.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: false
 
 module Applitools
-  VERSION = '4.6.3a'.freeze
-  IMAGES_VERSION = '4.2.3a'.freeze
-  UNIVERSAL_VERSION = '3.3.3a'.freeze
+  VERSION = '4.6.3'.freeze
+  IMAGES_VERSION = '4.2.3'.freeze
+  UNIVERSAL_VERSION = '3.3.3'.freeze
   UNIVERSAL_CORE_VERSION = '3.2.1'.freeze
 end
