07789f186b3a1975f9c4d9bcba38f491725c0bc5
SophieDePaula <sophie.tagar@applitools.com>
2023-07-24 13:07:12 -0700

    test: fix retries test

--- js/packages/eyes-cypress/test/fixtures/testApp/cypress/integration-run/retries.js
+++ js/packages/eyes-cypress/test/fixtures/testApp/cypress/integration-run/retries.js
@@ -3,6 +3,8 @@ describe('remove duplicate tests on retry', () => {
   it('fails and retries', {retries: 2}, () => {
     const retryCount = cy.state('runnable')._currentRetry
     cy.visit('https://example.org').then(() => {
+      // we wait here to make sure we get different time stamps for all tests
+      cy.wait(200)
       if (retryCount < 2) cy.document().then(doc => (doc.body.style.background = 'red'))
     })
     cy.eyesOpen({appName: 'cypress e2e', testName: 'cypress retries'})
