7b51ae7128efba32221627e69c4d291c1435f3c1
SophieDePaula <sophie.tagar@applitools.com>
2023-09-21 09:28:04 -0700

    test: run integration test from root installation

--- js/packages/eyes-cypress/test/it/browser/refer.test.js
+++ js/packages/eyes-cypress/test/it/browser/refer.test.js
@@ -1,23 +1,19 @@
 'use strict'
-const {describe, it, before} = require('mocha')
+const {describe, it} = require('mocha')
 const cypress = require('cypress')
-const path = require('path')
-const {exec} = require('child_process')
-const {promisify: p} = require('util')
-const pexec = p(exec)
+const targetDir = `test/it/browser/fixtures/`
+const cypressConfig = spec => {
+  return {
+    integrationFolder: 'test/it/browser/fixtures/cypress/integration',
+    testFiles: `${spec}.spec.js`,
+    video: false,
+    screenshotOnRunFailure: false,
+    pluginsFile: 'test/it/browser/fixtures/cypress/plugins/index-spec-driver-plugin.js',
+    supportFile: 'test/it/browser/fixtures/cypress/support/index-spec-driver-support.js',
+  }
+}
 
 describe('refer', () => {
-  before(async () => {
-    try {
-      process.chdir(path.resolve(__dirname, '../browser/fixtures'))
-      await pexec(`yarn`, {
-        maxBuffer: 1000000,
-      })
-    } catch (ex) {
-      console.log(ex)
-    }
-  })
-
   it('works for refer.spec.js', async () => {
     await runCypress('refer').then(results => {
       const tests = results.runs[0].tests
@@ -36,14 +32,16 @@ describe('refer', () => {
 
 function runCypress(spec) {
   return cypress.run({
-    browser: 'chrome',
+    browser: 'electron',
     headless: true,
-    spec: `cypress/integration/${spec}.spec.js`,
+    config: cypressConfig(spec),
+    configFile: `${targetDir}/cypress.json`,
   })
 }
 
 function openCypress() {
   return cypress.open({
-    browser: 'chrome',
+    browser: 'electron',
+    config: cypressConfig(spec),
   })
 }
