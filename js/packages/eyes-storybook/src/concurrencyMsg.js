b122504e3d8bf83c197df6759c803dcc815cf896
Amit Zur <amit.zur@applitools.com>
2023-10-03 08:09:05 +0300

    fix: docs for eyes setup and concurrency message output

--- js/packages/eyes-storybook/src/concurrencyMsg.js
+++ js/packages/eyes-storybook/src/concurrencyMsg.js
@@ -4,6 +4,6 @@ const chalk = require('chalk');
 module.exports = chalk.yellow(`
 Important notice: Your Applitools visual tests are currently running with a concurrency value of 5.
 This means that only up to 5 visual tests can run in parallel, and therefore the execution might be slower.
-If your Applitools license supports a higher concurrency level, learn how to configure it here: https://www.npmjs.com/package/@applitools/eyes-storybook#concurrency.
+If your Applitools license supports a higher concurrency level, set the "testConcurrency" parameter as explained here: https://applitools.com/docs/api-ref/sdk-api/storybook/config.
 Need a higher concurrency in your account? Email us @ sdr@applitools.com with your required concurrency level.
 `);
