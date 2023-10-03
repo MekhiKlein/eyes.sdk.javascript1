4e484871ee093cf265f82934d663d76c7a041553
Amit Zur <amit.zur@applitools.com>
2023-10-03 21:27:03 +0300

    chore: simplify updateConfigFile

--- js/packages/eyes-cypress/test/util/updateConfigFile.js
+++ js/packages/eyes-cypress/test/util/updateConfigFile.js
@@ -1,28 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-async function updateConfigFile({pluginFileName, testFile, targetTestAppPath}) {
-  const promise = new Promise(resolve => {
-    fs.readFile(path.resolve(targetTestAppPath, `./cypress.config.js`), 'utf-8', function (err, contents) {
-      if (err) {
-        console.log(err)
-        return
-      }
-
-      const replaced = contents
-        .replace(/.\/cypress\/plugins\/index-run.js/g, `${targetTestAppPath}/cypress/plugins/${pluginFileName}`)
-        .replace(/.\/cypress\/integration-run/g, `${targetTestAppPath}/cypress/integration-run/${testFile}`)
-        .replace(/.\/cypress\/support\/e2e.js/g, `${targetTestAppPath}/cypress/support/e2e.js`)
-
-      fs.writeFile(path.resolve(targetTestAppPath, `./cypress.config.js`), replaced, 'utf-8', function (err) {
-        if (err) {
-          console.log(err)
-        }
-        resolve()
-      })
-    })
-  })
-  await promise
+function updateConfigFile({pluginFileName, testFile, targetTestAppPath}) {
+  const configFilePath = path.resolve(targetTestAppPath, `./cypress.config.js`)
+  const contents = fs.readFileSync(configFilePath, 'utf-8')
+  const replaced = contents
+    .replace(/.\/cypress\/plugins\/index-run.js/g, `${targetTestAppPath}/cypress/plugins/${pluginFileName}`)
+    .replace(/.\/cypress\/integration-run/g, `${targetTestAppPath}/cypress/integration-run/${testFile}`)
+    .replace(/.\/cypress\/support\/e2e.js/g, `${targetTestAppPath}/cypress/support/e2e.js`)
+  fs.writeFileSync(configFilePath, replaced)
 }
 
 module.exports = updateConfigFile
